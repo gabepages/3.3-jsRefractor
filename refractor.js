5d5ef8a01396f771fe8d1b64c0dd60aae2ac624b
Gabriel Pages <gbpages@yahoo.com>
2016-02-17 13:40:25 -0500

    4th question answered

--- refractor.js
+++ refractor.js
@@ -111,6 +111,9 @@ var reverseStr = function(str) {
 
 // Put your answer below -------------------------
 
+var reverseStr = function(str) {
+  return str.split("").reverse().join("");
+};
 
 // -----------------------------------------------
 
